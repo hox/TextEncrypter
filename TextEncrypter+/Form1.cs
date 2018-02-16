b0fc115c4deeab0ec56f155f1779ccb18283135d
Caeser <ehernandez@evistaresources.com>
2018-02-15 22:11:47 -0500

    new file:   TextEncrypter+.sln 	new file:   TextEncrypter+/App.config 	new file:   TextEncrypter+/Form1.Designer.cs 	new file:   TextEncrypter+/Form1.cs 	new file:   TextEncrypter+/Form1.resx 	new file:   TextEncrypter+/Program.cs 	new file:   TextEncrypter+/Properties/AssemblyInfo.cs 	new file:   TextEncrypter+/Properties/Resources.Designer.cs 	new file:   TextEncrypter+/Properties/Resources.resx 	new file:   TextEncrypter+/Properties/Settings.Designer.cs 	new file:   TextEncrypter+/Properties/Settings.settings 	new file:   TextEncrypter+/TextEncrypter+.csproj 	new file:   TextEncrypter+/TextEncrypter+.csproj.user 	new file:   TextEncrypter+/bin/Debug/TextEncrypter+.exe 	new file:   TextEncrypter+/bin/Debug/TextEncrypter+.exe.config 	new file:   TextEncrypter+/bin/Debug/TextEncrypter+.pdb 	new file:   TextEncrypter+/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	new file:   TextEncrypter+/obj/Debug/TemporaryGeneratedFile_036C0B5B-1481-4323-8D20-8F5ADCB23D92.cs 	new file:   TextEncrypter+/obj/Debug/TemporaryGeneratedFile_5937a670-0e60-4077-877b-f7221da3dda1.cs 	new file:   TextEncrypter+/obj/Debug/TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs 	new file:   TextEncrypter+/obj/Debug/TextEncrypter+.csproj.CoreCompileInputs.cache 	new file:   TextEncrypter+/obj/Debug/TextEncrypter+.csproj.FileListAbsolute.txt 	new file:   TextEncrypter+/obj/Debug/TextEncrypter+.csproj.GenerateResource.Cache 	new file:   TextEncrypter+/obj/Debug/TextEncrypter+.csprojResolveAssemblyReference.cache 	new file:   TextEncrypter+/obj/Debug/TextEncrypter+.exe 	new file:   TextEncrypter+/obj/Debug/TextEncrypter+.pdb 	new file:   TextEncrypter+/obj/Debug/TextEncrypter_.Form1.resources 	new file:   TextEncrypter+/obj/Debug/TextEncrypter_.Properties.Resources.resources

---
+++ TextEncrypter+/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextEncrypter_
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string enc_code = "QsWaEzDxRfCtGvYhBuJnIkmOlP";
+
+            string toCompile = textBox1.Text.ToLower();
+            textBox1.Clear();
+            char[] c = toCompile.ToCharArray();
+            for (int x = 0; x < c.Length; x++)
+            {
+                for (int y = 0; y < 25; y++)
+                {
+                    if (c[x] == (char)(97 + y))
+                    {
+                        c[x] = enc_code[y];
+                        break;
+                    }
+                }
+            }
+            textBox2.Text = new string(c);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string enc_code = "QsWaEzDxRfCtGvYhBuJnIkmOlP";
+            string dec_code = "abcdefghijklmnopqrstuvwxyz";
+
+            string toCompile = textBox4.Text;
+            textBox4.Clear();
+            char[] c = toCompile.ToCharArray();
+            for (int x = 0; x < c.Length; x++)
+            {
+                for (int y = 0; y < 25; y++)
+                {
+                    if (c[x] == enc_code[y])
+                    {
+                        c[x] = dec_code[y];
+                        break;
+                    }
+                }
+            }
+            textBox3.Text = new string(c);
+        }
+    }
+}
